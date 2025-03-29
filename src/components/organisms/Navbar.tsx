25c3e9ade33499a96dfff6a944f41bdac5b94b1c
ErgunEmirhan <eergun321741@gmail.com>
2025-03-29 13:08:32 +0300

    linkedin and github hyperlinks added

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -1,6 +1,8 @@
-import { AppBar, Box, Button, Divider, Menu, MenuItem, MenuList, styled, Toolbar, Typography, useTheme } from '@mui/material'
+import { AppBar, Box, Button, Divider, MenuItem, MenuList, styled, Toolbar, Typography } from '@mui/material'
 import LanguageIcon from '@mui/icons-material/Language';
-import React, { Dispatch, SetStateAction, useContext } from 'react'
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { useContext } from 'react'
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { DarkModeContext } from '../../contexts/DarkModeContext';
@@ -16,12 +18,15 @@ const pages = ['Home', 'About', 'Projects']
 function Navbar() {
     
     const {isDarkMode, setIsDarkMode} = useContext<any>(DarkModeContext)
-    const theme = useTheme();
 
   return (
     <AppBar>
         <StyledToolbar>
-            <Typography variant='h6'>EMIRHAN ERGUN</Typography>
+            <Box>
+                <Typography variant='h6'>EMIRHAN ERGUN</Typography>
+            </Box>
+            <Divider orientation='vertical' variant='middle' flexItem/>
+           <Box>
                 <MenuList id='menu-appbar' sx={{display: 'flex', flexDirection: 'row'}}>
                     {pages.map((page) => (
                     <MenuItem key={page}>
@@ -29,19 +34,25 @@ function Navbar() {
                     </MenuItem>
                     ))}
                 </MenuList>
+           </Box>
+           <Divider orientation='vertical' variant='middle' flexItem/>
+
             <Box sx={{display: {xs: 'none', md: 'flex'}}}>
                <MenuList id='media-icons' sx={{display: {xs: 'none', md: 'flex'}}}>
                     <MenuItem>
-
+                        <Button onClick={() => window.open('https://www.linkedin.com/in/ergun-emirhan/')}><LinkedInIcon sx={{color:'white'}}/></Button>
+                    </MenuItem>
+                    <MenuItem>
+                        <Button onClick={() => window.open('https://github.com/ErgunEmirhan')}><GitHubIcon sx={{color:'white'}}/></Button>
                     </MenuItem>
                 </MenuList>
+            </Box>           
+            <Divider orientation='vertical' variant='middle' flexItem/>
+            <Box>
+                <Button><LanguageIcon sx={{color:'white'}}/></Button>
+                <Button onClick={() => setIsDarkMode(!isDarkMode)}>{!isDarkMode ? <LightModeIcon sx={{color:'white'}}/> : <DarkModeIcon sx={{color:'white'}}/>}</Button>
             </Box>
-            <Divider orientation='vertical' variant='middle' flexItem/>
-            
-            <Divider orientation='vertical' variant='middle' flexItem/>
-            <Button><LanguageIcon sx={{color:'white'}}/></Button>
-            <Button onClick={() => setIsDarkMode(!isDarkMode)}>{!isDarkMode ? <LightModeIcon sx={{color:'white'}}/> : <DarkModeIcon sx={{color:'white'}}/>}</Button>
-
+          
         </StyledToolbar>
     </AppBar>
   )
